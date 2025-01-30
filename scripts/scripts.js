0bdf94b81c0288f536d9c252bdf4089058df2ead
charlie-slime-sys <sawyer.pulsipher24@student.cart.org>
2025-01-30 15:01:29 -0800

    This was both hard and easy at the same time I sat there buffering

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,2 +1,28 @@
 const gootainer = document.getElementById("gootainer");
 const goocounter = document.getElementById("goocounter");
+
+let count = 0;
+let root = document.querySelector(":root")
+
+function make_goo() {
+    let new_goo = document.createElement("img")
+    new_goo.src = "low_res_googoobie.jpg";
+    count++;
+    goocounter.innerHTML = `GooGoobie Count: ${count}`;
+    gootainer.appendChild(new_goo);
+
+    let sqr = Math.sqrt(count);
+    if(sqr == Math.floor(sqr)){
+        root.style.setProperty("--grid", sqr + 1)
+    }
+
+    setTimeout(() => {
+        make_goo();
+    }, 10);
+}
+
+make_goo();
+
+// document.querySelector("body").addEventListener("mouseleave", (_) => {
+//     alert("why you leavin?")
+// })
